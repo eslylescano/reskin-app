aa486b9aac1ad6b51135c01d8e6a664f76b2b84b
esly <esly>
2020-08-26 23:05:09 +0100

    Adding screens for navigation

---
+++ app/screens/Models.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Models() {
+    return (
+        <View>
+            <Text>Models</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
