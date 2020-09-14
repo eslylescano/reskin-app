3fc4c38ee4a00c0cb6d2a94ed6682a628311823d
esly <esly>
2020-09-14 22:06:50 +0100

    Show and hide password

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,73 +1,76 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Input, Button, Icon } from 'react-native-elements'
-
+import React,{useState} from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Input, Button, Icon } from "react-native-elements";
 
 export default function RegisterForm() {
-    return (
-        <View style={styles.fromContainer}>
-            <Input
-            placeholder="Email"
-            containerStyle={styles.inputForm}
-            rightIcon={
-                <Icon
-                type="material-community"
-                name="at"
-                iconStyle={styles.iconRight}
-                />
-            }
-            />
-            <Input
-            placeholder="Password"
-            containerStyle={styles.inputForm}
-            password={true}
-            secureTextEntry={true}
-            rightIcon={
-                <Icon
-                type="material-community"
-                name="eye-outline"
-                iconStyle={styles.iconRight}
-                />
-            }
-            />
-            <Input
-            placeholder="Repeat Password"
-            containerStyle={styles.inputForm}
-            password={true}
-            secureTextEntry={true}
-            rightIcon={
-                <Icon
-                type="material-community"
-                name="eye-outline"
-                iconStyle={styles.iconRight}
-                />
-            }
-            />
-            <Button
-            title="Join"
-            containerStyle={styles.containerRegister}
-            buttonStyle={styles.btnRegister}
-            />
-        </View>
-    )
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+  return (
+    <View style={styles.fromContainer}>
+      <Input
+        placeholder="Email"
+        containerStyle={styles.inputForm}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
+      />
+      <Input
+        placeholder="Password"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={!showPassword}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+      />
+      <Input
+        placeholder="Repeat Password"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={!showRepeatPassword}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showRepeatPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+          />
+        }
+      />
+      <Button
+        title="Join"
+        containerStyle={styles.containerRegister}
+        buttonStyle={styles.btnRegister}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    fromContainer:{
-        marginTop:30
-    },
-    inputForm:{
-     width:"100%",
-     marginTop:20   
-    },
-    containerRegister:{
-        marginTop:20,
-        width:"95%"
-    },
-    btnRegister:{
-     backgroundColor:"#00a680"   
-    },
-    iconRight:{
-       color:"#c1c1c1" 
-    }
-})
+  fromContainer: {
+    marginTop: 30,
+  },
+  inputForm: {
+    width: "100%",
+    marginTop: 20,
+  },
+  containerRegister: {
+    marginTop: 20,
+    width: "95%",
+  },
+  btnRegister: {
+    backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
+  },
+});
